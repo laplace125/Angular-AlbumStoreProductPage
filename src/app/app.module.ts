406015c81034e9bb118e0d3bdd58f7852317674a
laplace125 <akanoafeez@gmail.com>
2020-10-10 17:16:43 +0100

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,7 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { RouterModule } from '@angular/router';
-import { Routes } from '@angular/router';
+import { RouterModule , Routes } from '@angular/router';
 
 
 const appRoutes: Routes = [
